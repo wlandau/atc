a62453956a98b27d2b5392fb3283b104e29681f4
wlandau <will.landau@gmail.com>
2014-08-09 00:47:15 -0400

    almost got it

--- plugins/ace-builds/src-noconflict/mode-atc.js
+++ plugins/ace-builds/src-noconflict/mode-atc.js
@@ -70,16 +70,16 @@ ace.define('ace/mode/atc_highlight_rules', ["require","exports","module","ace/li
           next : "line_comment_regex_allowed"
         }, {
           token: "wait", 
-          regex: /(^|,|[ \t]+)([Ww]|[Ww][Aa][Ii][Tt]|[Ww][Aa][Ii][Tt][Ii][Nn][Gg])($|[ \t]+)("[^",]*"?|[^ \t,]*)/
+          regex: /(^|,|[ \t]+)([Ww]|[Ww][Aa][Ii][Tt]|[Ww][Aa][Ii][Tt][Ii][Nn][Gg])($|[ \t]+)("[^",]*"?|'[^',]*'?|[^ \t,]*)/
         }, {
           token: "pend",
-          regex: /(^|,|[ \t]+)([Pp]|[Pp][Ee][Nn][Dd]|[Pp][Ee][Nn][Dd][Ii][Nn][Gg])($|[ \t]+)("[^",]*"?|[^ \t,]*)/
+          regex: /(^|,|[ \t]+)([Pp]|[Pp][Ee][Nn][Dd]|[Pp][Ee][Nn][Dd][Ii][Nn][Gg])($|[ \t]+)("[^",]*"?|'[^',]*'?|[^ \t,]*)/
         }, {
           token: "clear",
-          regex: /(^|,|[ \t]+)([Cc]|[Cc][Ll][Ee][Aa][Rr]|[Cc][Ll][Ee][Aa][Rr][Ee][Dd])($|[ \t]+)("[^",]*"?|[^ \t,]*)/
+          regex: /(^|,|[ \t]+)([Cc]|[Cc][Ll][Ee][Aa][Rr]|[Cc][Ll][Ee][Aa][Rr][Ee][Dd])($|[ \t]+)("[^",]*"?|'[^',]*'?|[^ \t,]*)/
         }, {
           token: "pass",
-          regex: /(^|,|[ \t]+)([Ss]|[Pp][Aa][Ss][Ss]|[Pp][Aa][Ss][Ss][Ee][Dd])($|[ \t]+)("[^",]*"?|[^ \t,]*)/
+          regex: /(^|,|[ \t]+)([Ss]|[Pp][Aa][Ss][Ss]|[Pp][Aa][Ss][Ss][Ee][Dd])($|[ \t]+)("[^",]*"?|'[^',]*'?|[^ \t,]*)/
         }, {
           token : "pos-number", 
           regex : /[+-]?\d+(?:(?:\.\d*)?(?:[eE][+-]?\d+)?)?\b/
