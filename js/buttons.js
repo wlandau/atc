83b3fbd899208819be98f29c880ff8b2f7837045
wlandau <will.landau@gmail.com>
2014-08-12 15:04:21 -0400

    got an EXCELLENT handle on all the keys now!

--- js/buttons.js
+++ js/buttons.js
@@ -1,34 +1,21 @@
 ATC.prototype.buttons = function(){
-  var atc = this;
+  var atc = this,
+      editor = ace.edit("editor");
 
-  var enableSave = function(){
+  enableSave = function(){
     var saveButton = $(".save");
     if(!saveButton.is(".no-save"))
       saveButton.removeClass("saved").addClass("unsaved");  
   };
+
+  editor.on("change", enableSave);
+  $(".atc > .header div").click(enableSave);
   
-  $(window).bind('keyup', function(e) {
-    var key = e.keyCode;
-    if(key === 8 || key === 46)
-      enableSave();
-  });
-
-
-$(document).keydown(function(e) {
-  var key = e.which || e.charCode || e.keyCode     
-       
-  if((e.ctrlKey || e.metaKey) && (key === 83 || key === 115)) {
-    e.preventDefault();
-    atc.save();
-  } else  if(!(e.ctrlKey || e.metaKey) && $(e.target).is(".ace_text-input")){
-    if(key === 8 ||
-       key === 9 ||
-       key === 13 ||
-       key === 32 ||
-       (key >= 46 && key <= 90) ||
-       (key >= 96 && key <= 111) ||
-       key >= 186)     
-      enableSave();   
+  $(document).keydown(function(e) {
+    var key = e.which || e.charCode || e.keyCode   
+    if((e.ctrlKey || e.metaKey) && (key === 83 || key === 115)){
+      e.preventDefault();
+      atc.save();
     }
   });
 
