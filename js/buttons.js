69ce310981be5b8bdf5d3c71ff9f264b589b2f16
wlandau <will.landau@gmail.com>
2014-08-12 14:56:35 -0400

    finally got a handle on the save key!

--- js/buttons.js
+++ js/buttons.js
@@ -6,26 +6,30 @@ ATC.prototype.buttons = function(){
     if(!saveButton.is(".no-save"))
       saveButton.removeClass("saved").addClass("unsaved");  
   };
-
-  $(".editor").keyup(function(e){
+  
+  $(window).bind('keyup', function(e) {
     var key = e.keyCode;
+    if(key === 8 || key === 46)
+      enableSave();
+  });
 
-    if(e.metaKey || e.ctrlKey)
-      return;
-    
+
+$(document).keydown(function(e) {
+  var key = e.which || e.charCode || e.keyCode     
+       
+  if((e.ctrlKey || e.metaKey) && (key === 83 || key === 115)) {
+    e.preventDefault();
+    atc.save();
+  } else  if(!(e.ctrlKey || e.metaKey) && $(e.target).is(".ace_text-input")){
     if(key === 8 ||
        key === 9 ||
        key === 13 ||
        key === 32 ||
        (key >= 46 && key <= 90) ||
        (key >= 96 && key <= 111) ||
-       key >= 186)
-       
-    enableSave();
-  });
-  
-  $(".atc > .header div").click(function(){
-    enableSave();
+       key >= 186)     
+      enableSave();   
+    }
   });
 
   $(".save").click(function(e){
