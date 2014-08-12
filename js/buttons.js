90d85c9e235634baa26e8035e24faa4dea1814a3
wlandau <will.landau@gmail.com>
2014-08-12 11:07:47 -0400

    delayed saving complete

--- js/buttons.js
+++ js/buttons.js
@@ -1,11 +1,6 @@
 ATC.prototype.buttons = function(){
   var atc = this;
 
-  var enableSave = function(){
-    if(atc.config.storage !== "none")
-      $(".save").removeClass("no-save saving saved").addClass("unsaved");  
-  };
-
   $(".editor").keyup(function(e){
     if(e.metaKey)
       return;
@@ -18,15 +13,11 @@ ATC.prototype.buttons = function(){
        (key >= 46 && key <= 90) ||
        (key >= 96 && key <= 111) ||
        key >= 186)
-    enableSave();
+       
+    atc.save("all");
   });
   
   $(".atc > .header div").click(function(){
-    enableSave();
-  });
-  
-  $(".save").click(function(){
-    if($(this).is(".unsaved"))
-      atc.save("all");
+    atc.save("config");
   });
 };
--- js/storage.js
+++ js/storage.js
@@ -1,11 +1,4 @@
 ;(function(localStorage, ATC){
-  
-  storeLocally = function(atc, saveType){
-    var editor = ace.edit("editor");
-    localStorage.setItem("ATCconfig", JSON.stringify(atc.config)); 
-    if(saveType === "all")
-      localStorage.setItem("ATCtext", editor.getSession().getValue()); 
-  };
 
   var retrieveLocally = function(atc){
     var editor = ace.edit("editor");    
@@ -14,22 +7,46 @@
     editor.moveCursorTo(0, 0);
   };
 
-  ATC.prototype.save = function(saveType){
-    $(".save").removeClass("no-save unsaved saved").addClass("saving");    
-    if(this.config.storage === "local")
-      storeLocally(this, saveType);
-    $(".save").removeClass("no-save unsaved saving").addClass("saved");
-  };
-  
   ATC.prototype.retrieve = function(){
     if(this.config.storage === "none") {
-      $(".save").removeClass("unsaved saving saved").addClass("no-save");
+      $(".save").removeClass("saving saved").addClass("no-save");
       return;
     } else if(this.config.storage === "local") {
+      $(".save").removeClass("no-save saving").addClass("saved");
       retrieveLocally(this);
     }
-    
-    $(".save").removeClass("no-save unsaved saving").addClass("saved");
   };
-})(window.localStorage, ATC);
+  
+  var storeLocally = function(atc, saveType){
+    var editor = ace.edit("editor");
+    localStorage.setItem("ATCconfig", JSON.stringify(atc.config)); 
+    if(saveType === "all" || saveType === undefined)
+      localStorage.setItem("ATCtext", editor.getSession().getValue()); 
+  };
+
+  var actuallySAVE = function(atc, callback, saveType){
+    if(atc.config.storage === "local")
+      storeLocally(atc, saveType);
+    callback();
+  }
+
+  var callback = function(){
+    var saveButton = $(".save");
+    if(saveButton.is(".saving"))
+      saveButton.removeClass("saving").addClass("saved");
+  }
+
+  ATC.prototype.save = function(saveType){
+    var atc = this,
+        doneTypingInterval = saveType === "all" ? 1000 : 100,
+        typingTimer;
+         
+    $(".save").removeClass(".saved").addClass("saving");
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(function(){
+      actuallySAVE(atc, callback, saveType)
+    }, doneTypingInterval);
+  };
+  
+})(window.localStorage, ATC, undefined);
   
