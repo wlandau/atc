3a28c45b9fa208d112b92d9938bd5ed06962ed65
wlandau <will.landau@gmail.com>
2014-08-19 17:52:48 -0500

    separated showStorage into its own file

---
+++ js/showStorage.js
@@ -0,0 +1,39 @@
+ATC.prototype.showStorage = function(){
+  var def = $.Deferred(),
+      editArea = $(".ace_content"),
+      editor = ace.edit("editor"),
+      saveButton = $(".save");
+  
+  setTimeout(function() {
+    saveButton.removeClass("no-save unsaved saving saved");
+
+    if(atc.storage.mode === "no-save")
+      saveButton.addClass("no-save");
+    else
+      saveButton.addClass(atc.storage.status);
+      
+    if(status === "saving"){
+      editArea.addClass("saving");
+      editor.setOptions({
+        readOnly: true, 
+        highlightActiveLine: false,
+        highlightGutterLine: false
+      });
+      editor.renderer.$cursorLayer.element.style.opacity=0
+      editor.textInput.getElement().disabled=true
+    } else if(status === "saved"){
+      editArea.removeClass("saving");
+      editor.setOptions({
+        readOnly: false,
+        highlightActiveLine: true,
+        highlightGutterLine: true
+      });
+      editor.renderer.$cursorLayer.element.style.opacity=1;
+      editor.textInput.getElement().disabled=false;
+      editor.focus();
+    }
+      
+    def.resolve();
+  }, 0);
+  return def.promise();   
+};
